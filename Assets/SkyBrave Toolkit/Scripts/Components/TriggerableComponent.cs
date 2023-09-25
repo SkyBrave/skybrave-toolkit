7a31b498a80d6b6f47b7dde847df8867c6899dd3
Yigit Doruk <yigitdorukk20@gmail.com>
2023-09-25 16:47:51 +0200

    fix: new folders created for different types of components.

---
+++ Assets/SkyBrave Toolkit/Scripts/Components/TriggerableComponent.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SkyBrave_Toolkit.Scripts.Components
+{
+    public class TriggerableComponent : MonoBehaviour
+    {
+        public UnityEvent OnTriggerEnterEvent;
+        public UnityEvent OnTriggerStayEvent;
+        public UnityEvent OnTriggerExitEvent;
+        public UnityEvent OnTriggerEnter2DEvent;
+        public UnityEvent OnTriggerStay2DEvent;
+        public UnityEvent OnTriggerExit2DEvent;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            OnTriggerEnterEvent.Invoke();
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            OnTriggerStayEvent.Invoke();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            OnTriggerExitEvent.Invoke();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            OnTriggerEnter2DEvent.Invoke();
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            OnTriggerStay2DEvent.Invoke();
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            OnTriggerExit2DEvent.Invoke();
+        }
+    }
+}
